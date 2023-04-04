1bcec4fe048c2364d616d652099ee6e98d006dbc
ANDRnewHuman <blsckcollapse@gmail.com>
2023-04-04 22:22:02 +0300

    11

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -8,3 +8,28 @@ import SimpleLightbox from 'simplelightbox';
 console.log(SimpleLightbox);
 // Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
+const refs = {
+  galleryTagUl: document.querySelector('.gallery'),
+};
+const cardsMarkup = createImgCardsMarkup(galleryItems);
+refs.galleryTagUl.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createImgCardsMarkup() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" 
+  href="${original}">
+    <img 
+    class="gallery__image" 
+    src="${preview}" 
+    alt="${description}" />
+  </a>
+</li>`;
+    })
+    .join('');
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  animationSpeed: 250,
+});
